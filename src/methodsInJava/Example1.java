075cee7b07ad9a161a5feff93d0f6a63ace1a718
pranay <pranayramteke163@gmail.com>
2024-11-05 03:26:02 -0800

    Getting Started With Methods In Java

---
+++ src/methodsInJava/Example1.java
@@ -0,0 +1,40 @@
+package methodsInJava;
+
+import java.util.Scanner;
+
+public class Example1 {
+
+    public static float volumeOfCuboid(float l, float b, float h)
+    {
+        // Logical Implementation Of Method
+
+        // calculating the volume of cuboid
+        float volume = l * b * h;
+        return volume;
+    }
+
+    public static void sayHello(String name)
+    {
+        System.out.println("Hello : " + name);
+        return;
+    }
+
+    public static void main(String[] args)
+    {
+        Scanner Input = new Scanner(System.in);
+        float len;
+        float bre;
+        float hei;
+        float volume;
+
+        System.out.println("Enter the l, b and h  : ");
+        len = Input.nextFloat();
+        bre = Input.nextFloat();
+        hei = Input.nextFloat();
+
+        volume = volumeOfCuboid(len,bre,hei);
+        System.out.println("Volume : " + volume);
+
+        sayHello("Pranay");
+    }
+}
